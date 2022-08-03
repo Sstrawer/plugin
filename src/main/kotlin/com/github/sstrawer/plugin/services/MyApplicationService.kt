395fc6bb84b17091140138336780e9764654a387
GitHub Action <action@github.com>
2022-08-03 11:41:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/sstrawer/plugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/sstrawer/plugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.sstrawer.plugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.sstrawer.plugin.MyBundle
 
 class MyApplicationService {
 
